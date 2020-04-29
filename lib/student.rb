2be1961162654e3a6c9016aa1849022cf7b9764c
William Mittard <billymittard@ymail.com>
2020-04-29 19:16:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,12 @@ class Student < User
 
 attr_accessor :knowledge 
 
-def initialize
+def initialize #initializes knowledge with an empty array 
   @knowledge = []
 end 
 
-def learn(knowledge)
-  @knowledge << knowledge
+def learn(knowledge) #Takes an argument of a String of knowledge and shovels it to the students knowledge array  
+@knowledge << knowledge
 end 
 
 def knowledge
