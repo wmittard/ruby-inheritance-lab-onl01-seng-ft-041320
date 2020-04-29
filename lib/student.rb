eaba875e398e70f2e66e8f2cbcd0bfb7aeb45fe3
William Mittard <billymittard@ymail.com>
2020-04-29 19:16:56 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User 
 
+attr_accessor :knowledge 
+
+def initialize #initializes knowledge with an empty array 
+  @knowledge = []
+end 
+
+def learn(knowledge) #Takes an argument of a String of knowledge and shovels it to the students knowledge array  
+@knowledge << knowledge
+end 
+
+def knowledge
+  @knowledge
+end 
+  
 end
---
+++ lib/user.rb
@@ -0,0 +1,7 @@
+class User #parent class
+ 
+ 
+ attr_accessor :first_name, :last_name #gives me my getter and setter method 
+  
+
+end 
